320f3327d6d17a5d7989b378f814e56a307ddb73
Dallin Pool <dallinpool@gmail.com>
2024-11-15 02:10:03 -0700

    Working on building and buying.

--- core/src/main/java/io/github/interastra/message/messages/AddBaseMessage.java
+++
@@ -1,4 +0,0 @@
-package io.github.interastra.message.messages;
-
-public record AddBaseMessage(String planetName, String base, int currentNumBases) {
-}
---
+++ core/src/main/java/io/github/interastra/rest/callbacks/AddBaseCallback.java
@@ -0,0 +1,2 @@
+package io.github.interastra.rest.callbacks;public class AddBaseCallback {
+}
